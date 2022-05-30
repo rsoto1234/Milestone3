1667dcbae5387088900c73014992b17fd4fc7dd1
rsoto1234 <89556340+rsoto1234@users.noreply.github.com>
2022-05-29 22:19:43 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Milestone2Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.restockbtn = new System.Windows.Forms.Button();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.displayBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(103, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Small Boxes";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(103, 81);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(105, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Medium Boxes";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(103, 111);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(89, 19);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Large Boxes";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(103, 141);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(101, 19);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "X-Large Boxes";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(251, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(564, 124);
+            this.listBox1.TabIndex = 4;
+            // 
+            // restockbtn
+            // 
+            this.restockbtn.Location = new System.Drawing.Point(171, 234);
+            this.restockbtn.Name = "restockbtn";
+            this.restockbtn.Size = new System.Drawing.Size(104, 46);
+            this.restockbtn.TabIndex = 5;
+            this.restockbtn.Text = "Restock Inventory";
+            this.restockbtn.UseVisualStyleBackColor = true;
+            this.restockbtn.Click += new System.EventHandler(this.restockbtn_Click);
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(229, 305);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(104, 46);
+            this.exitbtn.TabIndex = 6;
+            this.exitbtn.Text = "Exit";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(35, 236);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(103, 42);
+            this.addBtn.TabIndex = 7;
+            this.addBtn.Text = "Add Item";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(311, 234);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(98, 46);
+            this.searchBtn.TabIndex = 8;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // displayBtn
+            // 
+            this.displayBtn.Location = new System.Drawing.Point(444, 234);
+            this.displayBtn.Name = "displayBtn";
+            this.displayBtn.Size = new System.Drawing.Size(102, 44);
+            this.displayBtn.TabIndex = 9;
+            this.displayBtn.Text = "Display Inventory";
+            this.displayBtn.UseVisualStyleBackColor = true;
+            this.displayBtn.Click += new System.EventHandler(this.displayBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 412);
+            this.Controls.Add(this.displayBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.restockbtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Inventory Boxes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button restockbtn;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button displayBtn;
+    }
+}
